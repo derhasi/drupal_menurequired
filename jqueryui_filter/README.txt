e672e4e79b0f1c07c1f08e9f7492f1f9290a1224
derhasi <derhaside@gmail.com>
2010-03-13 00:34:32 +0100

    problem fixed with jquery_update 6.2-x

---
+++ jqueryui_filter/README.txt
@@ -0,0 +1,4 @@
+
+Dependencies:
+- Jquery-UI with at least JqueryUI 1.7.2
+- jquery_update-6.x-2.x-dev (1.x does not worl)
--- jqueryui_filter/jqueryui_filter_tabs.js
+++ jqueryui_filter/jqueryui_filter_tabs.js
@@ -9,10 +9,11 @@
  */
 Drupal.behaviors.jaueryui_filter_tabs = function(context) {
   for (var i in Drupal.settings.jqueryui_filter_tabs) {
-    var tabs_class = Drupal.settings.jqueryui_filter_tabs[i];
+    //var tabs_class = Drupal.settings.jqueryui_filter_tabs[i];
+    var tabs_class = i;
     $('div.jqueryui-filter-tabs.' + tabs_class + ':not(.jqueryui-filter-tabs-processed)').each(function () {
-      $(this).tabs();
       $(this).addClass('jqueryui-filter-tabs-processed');
+      $(this).tabs();
     });
   }
 };
